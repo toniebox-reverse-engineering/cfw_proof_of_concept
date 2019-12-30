37031362c0437f0a3079db574be36d526025ee7b
scilor <SciLor@users.noreply.github.com>
2019-12-30 16:25:19 +0100

    Added Power/DAC Reset pins. Increased speed

--- tb_leds/tb_leds/tb_leds.ino
+++ tb_leds/tb_leds/tb_leds.ino
@@ -1,9 +1,19 @@
 #define LED_RED 19 //19 TCK (??) //No digital pin, pin_layout unclear
-#define LED_GREEN 13 //21 SOP2 (13+36) //Works fine
-#define LED_BLUE 31 ///17 TDO (31+38) //Wiring unclear, signal needs to pull the led low put pulls it high.
+#define LED_GREEN 21 //21 SOP2 (13+36) //Works fine
+#define LED_BLUE 17 ///17 TDO (31+38) //Power Pin needed
+
+#define POWER_PIN 61 //61 GPIO06
+#define RESET_DAC_PIN 62 //62 GPIO07
 
 void setup() {
-  //pinMode(LED_RED, OUTPUT);
+
+  pinMode(RESET_DAC_PIN, OUTPUT);
+  pinMode(POWER_PIN, OUTPUT);
+  
+  digitalWrite(RESET_DAC_PIN, HIGH);
+  digitalWrite(POWER_PIN, HIGH);
+  
+  pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
   
@@ -12,21 +22,21 @@ void setup() {
 }
 
 void loop() {
-  //Serial.println("LOOP");
-  /*
+  Serial.println("LOOP");
+  
   digitalWrite(LED_RED, HIGH);
-  delay(500);
+  delay(250);
   digitalWrite(LED_RED, LOW);
-  delay(500);
-  */
+  delay(250);
+  
   digitalWrite(LED_GREEN, HIGH);
-  delay(500);
+  delay(250);
   digitalWrite(LED_GREEN, LOW);
-  delay(500);
+  delay(250);
   
   digitalWrite(LED_BLUE, HIGH);
-  delay(500);
+  delay(250);
   digitalWrite(LED_BLUE, LOW);
-  delay(500);
+  delay(250);
   
 }
