e2115594fd9a9d94c13e10a6b9bc0ad36162496e
scilor <SciLor@users.noreply.github.com>
2019-12-30 16:26:13 +0100

    Implemented Power / Reset pin and leds

--- i2c_test/i2c_test.ino
+++ i2c_test/i2c_test.ino
@@ -1,84 +1,59 @@
-// --------------------------------------
-// i2c_scanner
-//
-// Version 1
-//    This program (or code that looks like it)
-//    can be found in many places.
-//    For example on the Arduino.cc forum.
-//    The original author is not know.
-// Version 2, Juni 2012, Using Arduino 1.0.1
-//     Adapted to be as simple as possible by Arduino.cc user Krodal
-// Version 3, Feb 26  2013
-//    V3 by louarnold
-// Version 4, March 3, 2013, Using Arduino 1.0.3
-//    by Arduino.cc user Krodal.
-//    Changes by louarnold removed.
-//    Scanning addresses changed from 0...127 to 1...119,
-//    according to the i2c scanner by Nick Gammon
-//    https://www.gammon.com.au/forum/?id=10896
-// Version 5, March 28, 2013
-//    As version 4, but address scans now to 127.
-//    A sensor seems to use address 120.
-// Version 6, November 27, 2015.
-//    Added waiting for the Leonardo serial communication.
-//
-//
-// This sketch tests the standard 7-bit addresses
-// Devices with higher bit address might not be seen properly.
-//
+#define LED_RED 19 //19 TCK (??) //No digital pin, pin_layout unclear
+#define LED_GREEN 21 //21 SOP2 (13+36) //Works fine
+#define LED_BLUE 17 ///17 TDO (31+38) //Power Pin needed
+
+#define POWER_PIN 61 //61 GPIO06
+#define RESET_DAC_PIN 62 //62 GPIO07
  
 #include <Wire.h>
  
- 
 void setup()
 {
-  Wire.begin();
+  //pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+    
+  pinMode(RESET_DAC_PIN, OUTPUT);
+  pinMode(POWER_PIN, OUTPUT);
+  
+  digitalWrite(RESET_DAC_PIN, HIGH);
+  digitalWrite(POWER_PIN, HIGH);
+  
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_BLUE, HIGH);
+  delay(500);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_BLUE, LOW);
  
   Serial.begin(115200);
   while (!Serial);             // Leonardo: wait for serial monitor
+  
+  
+  Wire.begin();
   Serial.println("\nI2C Scanner");
 }
  
  
 void loop()
 {
-  byte error, address;
-  int nDevices;
- 
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address < 119; address++ )
+  Serial.print(" Scanning...");
+  uint8_t error, address;
+  for(address = 1; address < 127; address++ )
   {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
     Wire.beginTransmission(address);
+    Wire.write(1);  ///////////////////////////////////// sketch works ok when this line added
     error = Wire.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
+    if (error==0) {
+      Serial.print("Found Address 0x");
+      if (address<15) Serial.print('0');
       Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
+      Serial.print(", ");
     }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println(nDevices);
-
- while(1) { }
-  delay(5000);           // wait 5 seconds for next scan
+    delay(10);
+  }   
+   
+  Serial.println();  
+ //while(1) { }
+ 
+  delay(1000);           // wait 5 seconds for next scan
 }
