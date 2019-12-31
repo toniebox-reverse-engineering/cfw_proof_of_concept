1ab8978148f8db918d38f3238c58b69b53acb002
scilor <SciLor@users.noreply.github.com>
2019-12-31 15:25:57 +0100

    Added SWD Test to set pinmode right

---
+++ swd_test/swd_test.ino
@@ -0,0 +1,9 @@
+void setup() {
+    MAP_PinModeSet(PIN_19, PIN_MODE_1); //TCK
+    MAP_PinModeSet(PIN_20, PIN_MODE_1); //TMS
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
